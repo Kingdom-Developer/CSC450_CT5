4b71cafa476253d84ff85a61545f5692c47b4c54
Nolan Hill <nolanhill-developer@pm.me>
2025-12-14 14:51:57 -0700

    Final program that includes reverseFile method and main method to append user input to existing file and call on reverseFile method.

--- CT5/src/CT5.cpp
+++ CT5/src/CT5.cpp
@@ -3,14 +3,89 @@
 // Author      : Nolan Hill
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : User Input Program in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
+/*
+ * Method to create new file that has all characters reversed from the input file
+ */
+int reverseFile(string originalFile, string reverseFile) {
+	// Create input and output streams for file to read and file to write to
+	ifstream fileToReverse(originalFile);
+	ofstream reversedFile(reverseFile);
+
+	// Ensure original file opened correctly
+	if (!fileToReverse.is_open()) {
+		cout << "Unable to open original file.";
+		return 1;
+	}
+
+	// Ensure new reverse file opened correctly
+	if (!reversedFile.is_open()) {
+			cout << "Unable to open new file.";
+			return 2;
+	}
+
+	// Declare temporary variable to hold each line of original file
+	string line;
+
+	// Copy each line as elements in vector
+	while (getline(fileToReverse, line)) {
+		// Check if line is blank and start newline
+		if (line.empty()) {
+		    reversedFile << '\n';
+		    continue;
+		}
+
+		// Iterate through each line, starting at the end and writing to new file
+		for (int i = (int)line.size() - 1; i >= 0; i--) {
+			reversedFile << line[i];
+		}
+		// End each line with newline
+		reversedFile << '\n';
+	}
+
+	// Close both file streams
+	fileToReverse.close();
+	reversedFile.close();
+
+	return 0;
+}
+
+/*
+ * Demonstrates appending user input to end of a file and
+ * calls on reverseFile method to create new file with reversed
+ * characters.
+ */
 int main() {
-	cout << "Initial program." << endl; // prints Hello
+	string userInput;
+
+    // Create output file stream
+	ofstream fileToUpdate("CSC450_CT5_mod5.txt", ios::app);
+
+	// Check if file opened correctly
+	if (!fileToUpdate.is_open()) {
+		cout << "Unable to open file.";
+		return 1;
+	}
+
+	// Prompt user for input and store to variable
+	cout << "Enter text to add to file: ";
+	getline(cin, userInput);
+
+	fileToUpdate << userInput << endl;
+
+	// Close file stream
+	fileToUpdate.close();
+
+	// Call on method to create a copy of a file with reversed text
+	reverseFile("CSC450_CT5_mod5.txt", "CSC450-mod5-reverse.txt");
+
 	return 0;
 }
