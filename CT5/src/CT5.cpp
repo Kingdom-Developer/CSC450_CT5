650909b757d238705a58707d6a0a3be6bcaaf238
Nolan Hill <nolanhill-developer@pm.me>
2025-12-14 12:52:17 -0700

    Initial commit.

---
+++ CT5/src/CT5.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : CT5.cpp
+// Author      : Nolan Hill
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+	cout << "Initial program." << endl; // prints Hello
+	return 0;
+}
